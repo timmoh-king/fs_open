bd37792dcabcc0e4fc74c6ad0bb1b980aa5be35f
timmoh-king <timothykariuki43@gmail.com>
2023-07-12 10:40:59 +0300

    Deployment to production

--- backend/index.js
+++ backend/index.js
@@ -29,32 +29,32 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post("/api/notes", (request, response) => {
-  const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: "content missing" });
-  }
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  });
+  })
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+
+    .catch(error => next(error))
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
+  const { content, important } = request.body
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
     .then(updatedNote => {
       response.json(updatedNote)
     })
